d9d8cfa1da0c3b8cba6db75afe7332ec0c822a23
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2019-04-14 20:33:54 +0300

    Simple test

--- UnitTests/LibraryFileTests.cs
+++ UnitTests/LibraryFileTests.cs
@@ -1,4 +1,9 @@
 using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using NativeLibraryManager;
+using Shouldly;
 using Xunit;
 
 namespace UnitTests
@@ -6,8 +11,16 @@ namespace UnitTests
     public class LibraryFileTests
     {
         [Fact]
-        public void Test1()
+        public void CanUnpackFile()
         {
+            var file = new LibraryFile("TestFile.txt", SampleData.SampleFile);
+
+            string unpackedFile = file.UnpackResources();
+            
+            unpackedFile.ShouldNotBeNullOrEmpty();
+            File.Exists(unpackedFile).ShouldBeTrue();
+            File.ReadAllText(unpackedFile).ShouldBe(Encoding.UTF8.GetString(SampleData.SampleFile));
+            File.Delete(unpackedFile);
         }
     }
 }
---
+++ UnitTests/SampleData.cs
@@ -0,0 +1,9 @@
+using System.Text;
+
+namespace UnitTests
+{
+    public static class SampleData
+    {
+        public static readonly byte[] SampleFile = Encoding.UTF8.GetBytes("This is a test.");
+    }
+}
