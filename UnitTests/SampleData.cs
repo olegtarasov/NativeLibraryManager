f87ef15043903220ef81b1986e38c857edb0462f
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2020-05-24 10:48:17 +0300

    Removed unused file

--- UnitTests/SampleData.cs
+++
@@ -1,9 +0,0 @@
-using System.Text;
-
-namespace UnitTests
-{
-    public static class SampleData
-    {
-        public static readonly byte[] SampleFile = Encoding.UTF8.GetBytes("This is a test.");
-    }
-}
